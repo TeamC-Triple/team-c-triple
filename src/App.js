f7eed20343a9b0123bcf877cba2496c74a33edb1
seon022 <seon022@icloud.com>
2024-01-17 17:10:49 +0900

    Seonyoung Start

--- src/App.js
+++ src/App.js
@@ -8,17 +8,14 @@ import Home from './pages/Home.js';
  
 
 import dummyTouristSpots from './api/data_TouristSpots.js';
-import dummyTravelog from './api/data_Travelog.js';
 
 export const RecoCourseDataContext = React.createContext();
 export const SpotsDataContext = React.createContext();
-export const TravelogContext = React.createContext();
 
 function App() {
   return (
     <RecoCourseDataContext.Provider value={dummyRecoCourse}>
         <SpotsDataContext.Provider value={dummyTouristSpots}>
-            <TravelogContext.Provider value={dummyTravelog} >
                 <BrowserRouter>
                     <div className="App">
                         <Routes>
@@ -26,7 +23,6 @@ function App() {
                         </Routes>
                     </div>
                 </BrowserRouter>
-            </TravelogContext.Provider>
         </SpotsDataContext.Provider>
     </RecoCourseDataContext.Provider>
   );
--- src/home_subcomp/Best10.js
+++ src/home_subcomp/Best10.js
@@ -3,11 +3,16 @@ import BoxItem from "./BoxItem";
 const Best10 = ({SpotsData})=> {
     return(
         <div className="Best10">
-                {SpotsData.filter((it) => it.ranking % 2 === 1).sort((a, b) => a.ranking - b.ranking).map((it)=> <BoxItem key={it.id} {...it} />)
-                }
-                {SpotsData.filter((it) => it.ranking % 2 === 0).sort((a, b) => a.ranking - b.ranking).map((it)=> <BoxItem key={it.id} {...it} />)
-                }
-                
+            <div className="BestList">
+                <div className="row">
+                    {SpotsData.filter((it) => it.ranking % 2 === 1).sort((a, b) => a.ranking - b.ranking).map((it)=> <BoxItem key={it.id} {...it} />)
+                    }
+                </div>
+                <div className="row">
+                    {SpotsData.filter((it) => it.ranking % 2 === 0).sort((a, b) => a.ranking - b.ranking).map((it)=> <BoxItem key={it.id} {...it} />)
+                    }
+                </div>
+            </div>
         </div>
     );
 }
