108a487e8b438abad273238d659004b7ef58eb26
seon022 <seon022@icloud.com>
2024-01-18 15:37:18 +0900

    MFSec2 완료_선영

--- src/api/data_Travelog.js
+++ src/api/data_Travelog.js
@@ -23,7 +23,7 @@ export const dummyTravelog = [
             lastDay : new Date('2024-1-1').getTime()
         },
         city : '',
-        ment : '공휴일이 아까워서 간 여행이었는데 놀다보니 일출 보고 가고 싶어서 운좋게 1/1 기차표 구해서 일출도 보고 왔어요.'
+        ment : '공휴일이 아까워서 간 여행이었는데 놀다보니 일출 보고 가고 싶어서 운좋게 1/1 비행기표 구해서 일출도 보고 왔어요.'
      },
      {
         id : 2,
@@ -32,10 +32,29 @@ export const dummyTravelog = [
         profileImg : '/profile2.JPG',
         travelImg : ['/travelPhoto_11.jpg', '/travelPhoto_12.jpg', '/travelPhoto_13.jpg', '/travelPhoto_14.jpg', '/travelPhoto_15.jpg'],
         date : {
-            firstDay : new Date('2023-12-29').getTime(),
-            lastDay : new Date('2023-12-31').getTime()
+            firstDay : new Date('2023-12-01').getTime(),
+            lastDay : new Date('2023-12-03').getTime()
         },
         city : '',
         ment : '아이를 위한 아이로 인한 아이가 행복한 여행'
      }
 ];
+
+
+/*
+
+     {
+        id : 3,
+        title: '여자혼자 연말 제주여행',
+        writer : 'user',
+        profileImg : '/profile2.JPG',
+        travelImg : ['/travelPhoto_16.jpg', '/travelPhoto_17.jpg', '/travelPhoto_18.jpg', '/travelPhoto_19.jpg', '/travelPhoto_1.jpg'],
+        date : {
+            firstDay : new Date('2023-12-15').getTime(),
+            lastDay : new Date('2023-12-18').getTime()
+        },
+        city : '',
+        ment : '연말 제주여행을 결정하게된 이유는 바로 버킷리스트인 혼자 여행을 이루기 위해서! 혼자 빠르게 슝- 떠났습니다.'
+     }
+
+*/ 
--- src/home_subcomp/TravelogItem.js
+++ src/home_subcomp/TravelogItem.js
@@ -10,17 +10,17 @@ const TravelogItem = ({id, title, writer, profileImg, travelImg, date, ment})=> 
     return(
         <li className="TravelogItem">
             <div className="info">
-                <div className="profile"><img src={`./assets/travelPhoto${profileImg}`} /></div>
+                <div className="profile"><img src={`/assets/travelogPhoto${profileImg}`} /></div>
                 <div className="postInfo">
                     <p className="title">{title}</p>
                     <p className="writer">{writer}님의 일정 <span></span></p>
                 </div>
             </div>
             <div className="photo">
-                    <div className="mainImg"><img src={`assets/travelPhoto${travelImg[0]}`} alt="여행사진" /></div>
+                    <div className="mainImg"><img src={`/assets/travelogPhoto${travelImg[0]}`} alt="여행사진" /></div>
                     <div className="smallImg">
                         {travelImg.slice(1).map((it)=>(
-                            <p key={it}><img src={`./assets/travelPhoto${it}`} /></p>
+                            <p key={it}><img src={`/assets/travelogPhoto${it}`} /></p>
                         ))}
                     </div>
             </div>
