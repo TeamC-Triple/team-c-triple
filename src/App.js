50a2b764035455108fcf484f715fbf1dc6c62d40
nojimin <sh7606555@gmail.com>
2024-01-19 12:53:17 +0900

    MTS2

--- src/App.js
+++ src/App.js
@@ -1,28 +1,32 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
-import dummyMagazine from './api/dummyMegazine.js';
+import { dummyMyTripPlan } from './api/data_myTripPlan.js'
+import { dummyMagazine } from './api/dummyMegazine.js';
 import { dummyRecoCourse } from './api/data_recommendCourse.js';
 
 import './App.css';
 import Home from './pages/Home.js';
 
+export const MTLDataContext = React.createContext();
 export const RecoCourseDataContext = React.createContext();
 export const MagazineDataContext = React.createContext();
 
 function App() {
     return (
-      <MagazineDataContext.Provider value={dummyMagazine}>
-        <RecoCourseDataContext.Provider value={dummyRecoCourse}>
-            <BrowserRouter>
-                <div className="App">
-                    <Routes>
-                        <Route path='/' element={<Home />} />
-                    </Routes>
-                </div>
-            </BrowserRouter>
-        </RecoCourseDataContext.Provider>
-      </MagazineDataContext.Provider>
+        <MTLDataContext.Provider value={dummyMyTripPlan}>
+            <MagazineDataContext.Provider value={dummyMagazine}>
+                <RecoCourseDataContext.Provider value={dummyRecoCourse}>
+                    <BrowserRouter>
+                        <div className="App">
+                            <Routes>
+                                <Route path='/' element={<Home />} />
+                            </Routes>
+                        </div>
+                    </BrowserRouter>
+                </RecoCourseDataContext.Provider>
+            </MagazineDataContext.Provider>
+        </MTLDataContext.Provider>
   );
 }
 
---
+++ src/api/data_myTripPlan.js
@@ -0,0 +1,39 @@
+// data_myTripList.js
+
+export const dummyMyTripPlan = [
+    {
+        id: 0,
+        city: '부산',
+        firstDate: new Date(2024, 3, 1).getTime(),
+        lastDate: new Date(2024, 3, 2).getTime(),
+        photo:[],
+    },
+    {
+        id: 1,
+        city: '강릉',
+        firstDate: new Date(2024, 3, 6).getTime(),
+        lastDate: new Date(2024, 3, 7).getTime(),
+        photo:[],
+    },
+    {
+        id: 2,
+        city: '제주',
+        firstDate: new Date(2024, 3, 12).getTime(),
+        lastDate: new Date(2024, 3, 15).getTime(),
+        photo:[],
+    },
+    {
+        id: 3,
+        city: '경주',
+        firstDate: new Date(2024, 3, 17).getTime(),
+        lastDate: new Date(2024, 3, 23).getTime(),
+        photo:[],
+    },
+    {
+        id: 4,
+        city: '인천',
+        firstDate: new Date(2024, 4, 11).getTime(),
+        lastDate: new Date(2024, 4, 13).getTime(),
+        photo:[],
+    }
+]
