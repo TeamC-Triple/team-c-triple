a49d3ee455caa405d3f79d7b4e4bfde6667fa7a6
YunSeoyong <seoy2515@gmail.com>
2024-01-19 15:34:41 +0900

    mfs1 1차완성

--- src/home_subcomp/MyFeedItem.js
+++ src/home_subcomp/MyFeedItem.js
@@ -1,26 +1,140 @@
+import styled from "styled-components";
+
 const MyFeedItem = ({id, photo, title, firstDate, lasgDate, recoNum, commentNum, downloadNum}) => {
-    let firstMonth = new Date(firstDate).getMonth();
-    const getToDay = () => {
+    
+    const changeStringTD = (date) => {
+        const stringDay = new Date(date).getDay();
+        switch(stringDay){
+            case 0 : {return '일'
+                break;}
+            case 1 : {return '월'
+                break;}
+            case 2 : {return '화'
+                break;}
+            case 3 : {return '수'
+                break;}
+            case 4 : {return '목'
+                break;}
+            case 5 : {return '금'
+                break;}
+            case 6 : {return '토'
+                break;}
+            default : return stringDay;
+        };
+    };
+    
+    const onMyTravelDate = (date) => {
+        const stringDay = new Date(date).getDay();
+        const myTravelDate = new Date(date).toLocaleDateString();
+        const stringMTD = myTravelDate.slice(6, 11);
+        const mtd = `${stringMTD}(${changeStringTD(date)})`;
+        return mtd;
+    }
+
+    const truncate = (str, n) => {
+        return str && str.length > n
+            ? str.substring(0, n) + '...'
+            : str;
     };
 
     return (
-        <li className="MyFeedItem">
+        <MyFeedItemLi>
             <div className="myFeedItem_in">
-                <div className="myTravelLog">
-                    <p className="myl_icon">{photo}</p>
-                    <p className="myl_title">{title}</p>
-                    <p className="myl_date">
-                    </p>
-                    <p className="btn_edit">수정</p>
-                </div>
-                <div className="mt_social">
-                    <p className="social reco">{recoNum}</p>
-                    <p className="social comment">{commentNum}</p>
-                    <p className="social download">{downloadNum}</p>
-                </div>
+                <MyTravelLog>
+                    <div>    
+                        <MylIcon>{photo}</MylIcon>
+                        <MylTitle>{truncate(title, 10)}</MylTitle>
+                    </div>
+                    <div>
+                        <MylDate>
+                            {`${onMyTravelDate(firstDate)} - ${onMyTravelDate(lasgDate)}`}
+                        </MylDate>
+                        <GoEdit>수정</GoEdit>
+                    </div>
+                </MyTravelLog>
+                <MytSocial>
+                    <IconReco>{recoNum}</IconReco>
+                    <IconComment>{commentNum}</IconComment>
+                    <IconDownload>{downloadNum}</IconDownload>
+                </MytSocial>
             </div>
-        </li>
+        </MyFeedItemLi>
     );
 };
 
 export default MyFeedItem;
+
+const MyFeedItemLi = styled.li`
+    box-sizing: border-box;
+    width: 100%;
+    padding: 20px;
+    margin-bottom: 20px;
+    background-color: #fff;
+    border-radius: 10px;
+    box-shadow: 0 1px 8px rgba(0, 0, 0, 0.18);
+`;
+
+const MyTravelLog = styled.div`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 12px 10px;
+    margin-bottom: 10px;
+    background-color: #eee;
+    border-radius: 10px;
+    cursor: pointer;
+
+    & > div{
+        display: flex;
+        align-items: center;
+    }
+`;
+
+const MylIcon = styled.p`
+    width: 24px;
+    height: 24px;
+    margin-right: 5px;
+    background-color: #0BD0AF;
+    border-radius: 50%;
+`;
+const MylTitle = styled.p`
+    font-weight: 600;
+`;
+const MylDate = styled.p`
+    margin-right: 10px;
+    font-size: 11px;
+    letter-spacing: -0.5px;
+`;
+const GoEdit = styled.p`
+    font-size: 13px;
+    font-weight: 500;
+    cursor: pointer;
+`;
+
+const MytSocial = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    p{
+        margin: 0 24px;
+        padding-left: 28px;
+        font-weight: 500;
+        color: #121212;
+        background-repeat: no-repeat;
+        background-position: left center;
+        background-size: 20px auto;
+        opacity: 0.6;
+        cursor: pointer;
+    }
+`;
+
+const IconReco = styled.p`
+    background-image: url('/assets/icon-heart-line.svg');
+`;
+const IconComment = styled.p`
+    background-image: url('/assets/icon-chat.svg');
+`;
+const IconDownload = styled.p`
+    background-image: url('/assets/icon-download.svg');
+`;
--- src/homecomp/MFSection1.js
+++ src/homecomp/MFSection1.js
@@ -1,36 +1,62 @@
 import { useContext, useEffect, useState } from 'react';
 
-import './MFSection1.css'
 import { MyTripDataContext } from '../App.js';
 import MyFeedItem from '../home_subcomp/MyFeedItem.js';
 import Button from '../common/Button.js';
+import styled from 'styled-components';
 
 const MFSection1 = () => {
     const myTripList = useContext(MyTripDataContext);
     const [data, setData] = useState([]);
+    const [sliceNum, setSliceNum] = useState(2)
+    const ListSlice = sliceNum + (myTripList.length - sliceNum)
 
     useEffect(() => {
         setData(myTripList);
     }, [])
 
+    const clickListMore = () => {
+        setSliceNum(ListSlice);
+    };
+
     return (
-        <div className='MFSection1'>
-            <div className='mfSection1_in'>
+        <MFS1>
+            <MfSection1_in>
                 <h2 className='section_title'>
                     나의 여행 피드
                 </h2>
-                <div className='myFeedList'>
+                <MyFeedList>
                     <ul>
-                        {data.map((it) => <MyFeedItem key={it.id} {...it} />)}
+                        {data.slice(0,sliceNum).map((it) => <MyFeedItem key={it.id} {...it} />)}
                     </ul>
-                </div>
+                </MyFeedList>
                 <Button 
                     type={'gray_border'}
                     text={'나의 여행 피드 더보기'}
+                    onClick={clickListMore}
                 />
-            </div>
-        </div>
+            </MfSection1_in>
+        </MFS1>
     );
 };
 
 export default MFSection1;
+const MFS1 = styled.div`
+    margin: 80px 0 40px;
+`;
+
+const MfSection1_in = styled.div`
+    padding: 0 20px;
+
+    h2{
+        margin-bottom: 16px;
+    }
+    
+    button{
+        width: 100%;
+    }
+`;
+
+const MyFeedList = styled.div`
+    margin-bottom: 20px;
+`;
