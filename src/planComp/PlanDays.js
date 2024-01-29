fdccce1cdb84dcb19212a9dbce419fedb3cf31d3
seon022 <seon022@icloud.com>
2024-01-29 11:13:02 +0900

    planDays Start

--- src/planComp/PlanDays.js
+++ src/planComp/PlanDays.js
@@ -1,12 +1,19 @@
 import styled from "styled-components";
 
 const PlanDays = ({day, idx}) => {
+    const [isEmpty, setIsEmpty]= useState(true);
     return (
         <Plandays>
             <div className="pdy_top">
                 <h3>DAY {idx+1} <span>({day})</span></h3>
                 <p className="pdy_pay">예상 경비 : </p>
             </div>
+            {isEmpty ? <Empty>일정이 비어있습니다.</Empty> 
+                    :  <TourSpots />}
+            <Day>
+                <Button type={'gray_border'} text='장소추가' />
+                <Button type={'gray_border'} text='메모추가' />
+            </Day>
         </Plandays>
     );
 };
@@ -35,4 +42,15 @@ const Plandays = styled.div`
             color: #666;
         }
     }
-`;
+`
+const Day = styled.div`
+    display : flex;
+    justify-content: space-between;
+    .Button{
+        width: 49%;
+    }
+`
+const Empty = styled.div`
+    padding : 30px 0;
+    text-align:center;
+ `
