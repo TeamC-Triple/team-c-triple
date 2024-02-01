f9fa4ff18f7c3c2d69c350f1b34d02af007f5d68
YunSeoyong <seoy2515@gmail.com>
2024-02-01 17:14:39 +0900

    0201 17:14 calendar

--- src/pages/Plan.js
+++ src/pages/Plan.js
@@ -119,6 +119,7 @@ const Plan = () => {
         onCreatePlan(chosedCity, startDate, lastDate, keywordList, traveler, expenses);
     };
     
+    console.log(travelDateRange);
     
 
     return (
